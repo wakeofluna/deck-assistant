01e0968ec4fd54822e4c8160356ce513e3a27d5c
Esther Dalhuisen <wakeofluna@astralkey.nl>
2024-04-05 10:23:11 +0200

    Add GPLv3 headers to all code files

--- src/deck_logger.h
+++ src/deck_logger.h
@@ -1,3 +1,21 @@
+/*
+ * DeckAssistant - Creating control panels using scripts.
+ * Copyright (C) 2024  Esther Dalhuisen (Wake of Luna)
+ *
+ * DeckAssistant is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DeckAssistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef DECK_ASSISTANT_DECK_LOGGER_H
 #define DECK_ASSISTANT_DECK_LOGGER_H
 
